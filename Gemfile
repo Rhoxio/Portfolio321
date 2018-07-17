890f2fece810b794a05bdbb16fb5f8fcad5ac5fc
Rhoxio <rhoxiodbc@gmail.com>
2018-07-16 19:17:15 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+
+gem "selenium-webdriver"
+gem 'chromedriver-helper'
+gem 'awesome_print'
+gem 'nokogiri'
+gem 'time_diff'
+
+gem 'rspec'
---
+++ spec/portfolio_321_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Portfolio321 do
+  it "has a version number" do
+    expect(Portfolio321::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
